9b16e40c3d0e3cd43ba396b3becc0cc054a7f3ac
Pierre-Yves Martin <pym.aldebaran@gmail.com>
2018-03-01 12:18:04 +0100

    Fix the architecture to have an installable aias module and therefore a clean test structure.

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='aias',
+      version='0.0.1',
+      description='Text based adventure game with a procedural generation scenario. And everything takes place in a giant seashell.',
+      author='Pierre-Yves "Pirata" Martin',
+      author_email='pym.aldebaran@gmail.comt',
+      url='https://github.com/pymaldebaran/aias',
+      packages=['aias'],
+      install_requires=['begins'],
+      tests_requires=['pytest', 'pytest-bdd']
+     )
--- tests/test_aias.py
+++ tests/test_aias.py
@@ -4,22 +4,24 @@ from pytest_bdd import (
     when,
     then)
 
-@scenario('enter_the_shell.feature', 'Entering in the shell')
+import aias
+
+@scenario('features/enter_the_shell.feature', 'Entering in the shell')
 def test_entering_in_the_shell():
     pass
 
 @given('I am a player')
 def player():
     return 'Ze player'
-    
+
 @when('I enter the shell')
 def enter_shell():
     pass
-    
+
 @then('I have a character')
 def have_character():
     assert True
-    
+
 @then('I have a scenario')
 def have_scenario():
     assert True
